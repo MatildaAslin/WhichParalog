5c9690e836f3da1b9ba764d536da4075f212f36b
Vroni <vroni@Hugo.(none)>
2013-09-11 15:01:49 +0200

    Modified code for counting gaps - fixed bug at counting - compare the number of gaps in two sequences

--- findSplitCandidate.pl
+++ findSplitCandidate.pl
@@ -2,31 +2,109 @@
 
 use strict;
 use warnings;
-use Bio::Seq;
+use Bio::SeqIO;
+use Getopt::Long;
+use File::Slurp;
+use Bio::TreeIO;
+use IO::String;
+use Bio::AlignIO;
+use Bio::Align::ProteinStatistics;
+use Bio::Tree::DistanceFactory;
+use Bio::Tree::TreeI;
+use Bio::SimpleAlign;
 
+#Declaring variables
+my $input;
+my $seq;
+my @name; #get the name from the seq
+my @all; #all names
 
-my $seq_obj = Bio::Seq->new(-seq => "MGVDDRIEELEEKLKKTPVNKATEKERGRLKSQIAQLKEEKQKK--QKGTGETSGYAVEKTGDATVALVGFPSVGKSTLLNELTNTEVDTGAYEFTTLEVNPGVLKYKGANIQILDVPGLIGGAADGRGGGTQVLSVVRNADLILVVLDPEEL--RDDEIRDEVYKACLRTDSKPPNMKIEKKDRVG--------------------------------------------------------------------------------------------------YHLQLILILKRRQ-------------------------------------------------------------L",
-                         -alphabet => 'protein' );
-                         
-#get sequence as String object
-my $sequence = $seq_obj->seq;
+#Saving input parameters
+GetOptions ("i|input=s" => \$input);
 
-#split String to get each character individually
-my @splitSeq = split(//, $sequence);
+#Load alignment file
+my $str = Bio::AlignIO->new(-file =>$input, -format => 'fasta');
+my $aln = $str->next_aln();
 
-#counters for current number of gaps and maximum number of gaps
-my $gapCounter = 0;
-my $maxGaps = 0;
+$aln->sort_alphabetically;
 
-foreach my $char (@splitSeq) {
-    if ($char eq '-') { #count number of gaps in a row
-        ++$gapCounter;
-    } elsif ($gapCounter > $maxGaps) {
-        $maxGaps = $gapCounter;
-        $gapCounter = 0;
-    } else {
-        $gapCounter = 0;
-    }
+foreach $seq($aln->each_seq){
+	@name = split(/_/, $seq->id);
+	push(@all, $name[0].$name[1]);
 }
 
-print $maxGaps . "\n";
+for (my $i=1; $i<scalar(@all); $i++){
+	if ($all[$i] eq $all[$i+1]) {
+	    
+		#go through the genes and check if they can be split-genes, 
+		#save candidat incase more than one copie
+		#If it is a candidate, save the number i so it can be 
+		#removed it it actually is a split gene
+		#print $all[$i]."\n";
+	}
+}
+
+my @alnArray = $aln->each_seq;
+for (my $i=1; $i<scalar(@alnArray) - 1; $i++) {
+
+    #compare if species is the same, i.e. the names are equal
+    my @nameCurrent = split(/_/, $alnArray[$i]->id);
+    my @nameNext = split(/_/, $alnArray[$i+1]->id);
+    #species are defined by the first two words of their id
+	if ($nameCurrent[0].$nameCurrent[1] eq $nameNext[0].$nameNext[1]) {
+	    print $nameCurrent[0].$nameCurrent[1]." ".$nameNext[0].$nameNext[1]."\n";
+	    #print $alnArray[$i]->seq."\n".$alnArray[$i+1]->seq."\n";
+	    #compare the number of gaps in the sequences
+	    my $currentGaps = countGaps($alnArray[$i]->seq);
+	    my $nextGaps = countGaps($alnArray[$i+1]->seq);
+	    print $currentGaps . ", " . $nextGaps . "\n";
+	}
+}
+
+#Usefull code:
+#  	add seq					$myalgin->add_seq($newseq);
+# 	remove seq				$aln->remove_seq($seq);
+#	get seq by pos			$seq=$aln->get_seq_by_pos($i):
+#	number of genes			no_sequences()
+#	new alignment of seq 1-5			$aln2=$aln->select(1,5); 
+#	write new file			$aln->write_fasta(\*OUTPUT)
+#   					print $aln->length, "\n";
+#   					print $aln->no_residues, "\n";
+#   					print $aln->is_flush, "\n";
+#   					print $aln->no_sequences, "\n";
+#	$factory = Bio::Tools::Run::Alignment::Muscle->new(-format =>  
+#'fasta',  -verbose=>'', -quiet=>'', -log='inv.log');
+#	aln=$factory->align($inputfilename);
+
+#To-do list
+# Check if there are genes from the same organism.
+# Check these genes if they seem to be split. Check the length of gene. 
+# See how many gaps there are (some kind of count for where the gap is).
+# If gap on different places, put them together. Realigned.
+
+# make sure input is a sequence object. Count gaps in sequence
+sub countGaps {
+    my $sequence = $_[0];
+    #split String to get each character individually
+    my @splitSeq = split(//, $sequence);
+
+    #counters for current number of gaps and maximum number of gaps
+    my $gapCounter = 0;
+    my $maxGaps = 0;
+
+    foreach my $char (@splitSeq) {
+        if ($char eq '-') { #count number of gaps in a row
+            ++$gapCounter;
+        } elsif ($gapCounter > $maxGaps) {
+            $maxGaps = $gapCounter;
+            $gapCounter = 0;
+        } else {
+            $gapCounter = 0;
+        }
+    }
+    if ($gapCounter > $maxGaps) {
+            $maxGaps = $gapCounter;
+    }
+    
+    return $maxGaps;
+}
--- split.pl
+++ split.pl
@@ -34,12 +34,30 @@ foreach $seq($aln->each_seq){
 }
 
 for (my $i=1; $i<scalar(@all); $i++){
-	if ($all[$i] eq $all[$i+1]){
+	if ($all[$i] eq $all[$i+1]) {
+	    
 		#go through the genes and check if they can be split-genes, 
 		#save candidat incase more than one copie
 		#If it is a candidate, save the number i so it can be 
 		#removed it it actually is a split gene
-		print $all[$i]."\n";
+		#print $all[$i]."\n";
+	}
+}
+
+my @alnArray = $aln->each_seq;
+for (my $i=1; $i<scalar(@alnArray) - 1; $i++) {
+
+    #compare if species is the same, i.e. the names are equal
+    my @nameCurrent = split(/_/, $alnArray[$i]->id);
+    my @nameNext = split(/_/, $alnArray[$i+1]->id);
+    #species are defined by the first two words of their id
+	if ($nameCurrent[0].$nameCurrent[1] eq $nameNext[0].$nameNext[1]) {
+	    print $nameCurrent[0].$nameCurrent[1]." ".$nameNext[0].$nameNext[1]."\n";
+	    #print $alnArray[$i]->seq."\n".$alnArray[$i+1]->seq."\n";
+	    #compare the number of gaps in the sequences
+	    my $currentGaps = countGaps($alnArray[$i]->seq);
+	    my $nextGaps = countGaps($alnArray[$i+1]->seq);
+	    print $currentGaps . ", " . $nextGaps . "\n";
 	}
 }
 
@@ -63,3 +81,30 @@ for (my $i=1; $i<scalar(@all); $i++){
 # Check these genes if they seem to be split. Check the length of gene. 
 # See how many gaps there are (some kind of count for where the gap is).
 # If gap on different places, put them together. Realigned.
+
+# make sure input is a sequence object. Count gaps in sequence
+sub countGaps {
+    my $sequence = $_[0];
+    #split String to get each character individually
+    my @splitSeq = split(//, $sequence);
+
+    #counters for current number of gaps and maximum number of gaps
+    my $gapCounter = 0;
+    my $maxGaps = 0;
+
+    foreach my $char (@splitSeq) {
+        if ($char eq '-') { #count number of gaps in a row
+            ++$gapCounter;
+        } elsif ($gapCounter > $maxGaps) {
+            $maxGaps = $gapCounter;
+            $gapCounter = 0;
+        } else {
+            $gapCounter = 0;
+        }
+    }
+    if ($gapCounter > $maxGaps) {
+            $maxGaps = $gapCounter;
+    }
+    
+    return $maxGaps;
+}
