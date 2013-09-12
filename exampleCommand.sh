7f223f2289b1bf853db7a8f8fb6fb0d3c36a8e17
Matilda <Matilda@student-246-191.eduroam.uu.se>
2013-09-12 15:58:22 +0200

    TypoLocator and checkIfBacteria
    Added new subroutines typoLocator (detects typos in species names in
    the tree file) and CheckIfBacteria (checks if a node is bacterial).
    Also started to implement re-rooting and monophyly-checking. Added new
    tree file to example that contains bacteria.

--- exampleCommand.sh
+++ exampleCommand.sh
@@ -5,4 +5,4 @@ perl split.pl -i example/arCOG00358_pruned.fasta
 perl blast.pl -i example/arCOG00358_pruned.fasta
 
 #paralog.pl command
-perl paralog.pl -t example/arCOG00358_colored.nex -g example/organisms_groups.tab
+perl paralog.pl -t example/arCOG00415_colored.nex -g example/organisms_groups.tab
--- paralog.pl
+++ paralog.pl
@@ -11,6 +11,7 @@ use Bio::AlignIO;
 use Bio::Align::ProteinStatistics;
 use Bio::Tree::DistanceFactory;
 use Bio::Tree::TreeI;
+use Bio::Tree::TreeFunctionsI;
 use Bio::SimpleAlign;
 
 #Declaring variables
@@ -21,6 +22,8 @@ my %groups;
 my $speciesName;
 my %leavesName;
 my @paralogSpecies;
+my @bacteria;
+my @reBacteria;
 
 #Saving input parameters
 GetOptions ("t|treefile=s" => \$treefile, "g|groupfile=s" => \$groupfile);
@@ -33,10 +36,8 @@ foreach my $line (<INFILE>) {
 	chomp($line);
 		
 	my ($groupname, $protein) = split("\t", $line);
-	
+		
 	my @species = split("_", $protein);
-
-	#$groups{$species[0] . "_" . $species[1]} = $groupname;
 
 	$groups{$species[0]} = $groupname;
 }
@@ -48,11 +49,42 @@ my $tree = $str->next_tree();
 #Puts all leaves in an array
 my @leaves = $tree->get_leaf_nodes();
 
+for (@leaves){
+	if( checkIfBacteria($_) ){
+		push(@bacteria, $_);
+	}
+}
+
+#re-root tree with bacteria (the first one in the array)
+$tree->reroot($bacteria[0]); 
+
+#get leaves of re-rooted tree
+my @reLeaves = $tree->get_leaf_nodes();
+
+#gets bacterial nodes of re-rooted tree
+for (@reLeaves){
+	if( checkIfBacteria($_) ){
+		push(@reBacteria, $_);
+	}
+}
+
+my $lca = $tree->get_lca(-nodes => \@reBacteria);
+
+print $lca . "\n";
+	
+
 #Loops over all leaves and puts the speciesnames in hash
 foreach (@leaves){
+	
 	my $proteinID = $_->id;
-	my @id = split("_", $proteinID);
+	
+	my @id = split("_", $proteinID) ;
+	
+	#Checks that all species names coorrespond to the ones in the groupfile
+	typoLocator($id[0]) or die "ERROR: Typo (" . $id[0] . ") in treefile: " . $treefile . "\n Please correct this before running Tree Doctor\n" ;
+	
 	$speciesName = $id[0] . "_" . $id[1];
+	
 	$leavesName{$speciesName}++;
 }
 
@@ -63,6 +95,29 @@ for (keys %leavesName){
 	}
 }
 
+print "Paralog species:\n";
 for(@paralogSpecies){
 	print $_ . "\n";
+}
+
+#For debugging
+#foreach my $key (keys %leavesName){
+#	my @damp = split("_", $key); 
+#	print $damp[0] . " => " . $groups{$damp[0]} . "\n";
+#}
+
+sub checkIfBacteria{
+	my $nodeName = $_[0]->id;
+	my @splitSpecies = split("_", $nodeName);
+	
+	if ($groups{$splitSpecies[0]} eq "bact"){
+		return 1;
+	}
+}
+
+sub typoLocator{
+	
+	if ($groups{$_[0]}){
+		return 1;
+	}
 }
