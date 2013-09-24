228d87fb353f05a3efdb30f49de141aa59527b1d
Vroni <vroni@Hugo.(none)>
2013-09-24 13:41:01 +0200

    Minor changes in split script.

--- split.pl
+++ split.pl
@@ -28,22 +28,6 @@ my $aln = $str->next_aln();
 
 $aln->sort_alphabetically;
 
-foreach $seq($aln->each_seq){
-	@name = split(/_/, $seq->id);
-	push(@all, $name[0].$name[1]);
-}
-
-for (my $i=1; $i<scalar(@all) - 1; $i++){
-	if ($all[$i] eq $all[$i+1]) {
-	    
-		#go through the genes and check if they can be split-genes, 
-		#save candidat incase more than one copie
-		#If it is a candidate, save the number i so it can be 
-		#removed it it actually is a split gene
-		#print $all[$i]."\n";
-	}
-}
-
 my @alnArray = $aln->each_seq;
 for (my $i=1; $i<scalar(@alnArray) - 1; $i++) {
 
@@ -52,22 +36,26 @@ for (my $i=1; $i<scalar(@alnArray) - 1; $i++) {
     my @nameNext = split(/_/, $alnArray[$i+1]->id);
     #species are defined by the first two words of their id
 	if ($nameCurrent[0].$nameCurrent[1] eq $nameNext[0].$nameNext[1]) {
-	    my $currentAln = $alnArray[$i]->seq; # This removing the '-' charachter
-        my $nextAln = $alnArray[$i+1]->seq;  # and then adding it again is not very pretty :(
+	    my $currentAln = $alnArray[$i]->seq;
+        my $nextAln = $alnArray[$i+1]->seq;
         
         #make bitwise or to find overlaps
         my $or = $currentAln | $nextAln;
         $or =~ s/ /-/g;
         (my $overlap = $or) =~ s/[^A-Z]//g;
         
-        if (length $overlap <= 10) { # What overlap threshold is good?
+        if (length $overlap <= 5) { # What overlap threshold is good?
         # calculate mean of alignment
             if (calcMean($currentAln) < calcMean($nextAln)) {
                 $alnArray[$i]->seq($currentAln . $nextAln);
                 $aln->remove_seq($alnArray[$i+1]);
+                print $nameCurrent[0].$nameCurrent[1] . ":\n" . $currentAln . "\n";
+                print $nameNext[0].$nameNext[1] . ":\n" . $nextAln . "\n";
             } else {
                 $alnArray[$i]->seq($nextAln . $currentAln);
                 $aln->remove_seq($alnArray[$i+1]);
+                print $nameCurrent[0].$nameCurrent[1] . ":\n" . $currentAln . "\n";
+                print $nameNext[0].$nameNext[1] . ":\n" . $nextAln . "\n";
             }
         }
 	}
